Fixed compas label scaling

scripts/main.py
@@ -64,7 +64,8 @@ class Net(nn.Module):
         self._grl_lambda = grl_lambda
         self.fc1 = nn.Linear(input_shape, 64)
         self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(32, 32)
+        self.fc3a = nn.Linear(32, 64)
         self.fc4 = nn.Linear(64, 1)
         if self._grl_lambda != 0:
             self.grl = GradientReversal(grl_lambda)
@@ -81,6 +82,10 @@ class Net(nn.Module):
         hidden = F.dropout(hidden, 0.1)
 
         hidden = self.fc3(hidden)
+        hidden = F.relu(hidden)
+        hidden = F.dropout(hidden, 0.1)
+
+        hidden = self.fc3a(hidden)
         hidden = F.relu(hidden)
         hidden = F.dropout(hidden, 0.1)
 
@@ -155,7 +160,7 @@ def train_and_evaluate(train_loader: DataLoader,
 
     criterion = nn.MSELoss().to(device)
     criterion_bias = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
     scheduler = ReduceLROnPlateau(optimizer, threshold=0.3, cooldown=5)
 
     training_losses = []
@@ -278,12 +283,14 @@ def main(args):
     if args.dataset == "compas":
         df = pd.read_csv(os.path.join("..", "data", "csv", "scikit",
                                       "compas_recidive_two_years_sanitize_age_category_jail_time_decile_score.csv"))
-        df_binary, Y, S, _ = transform_dataset(df)
+        df_binary, Y, S, Y_true = transform_dataset(df)
         Y = Y.to_numpy()
+        l_tensor = torch.tensor(Y_true.to_numpy().reshape(-1, 1).astype(np.float32))
     elif args.dataset == "adult":
         ##load the census income data set instead of the COMPAS one
         df = pd.read_csv(os.path.join("..", "data", "csv", "scikit", "adult.csv"))
         df_binary, Y, S, _ = transform_dataset_census(df)
+        l_tensor = torch.tensor(Y.reshape(-1, 1).astype(np.float32))
     else:
         raise ValueError(
             "The value given to the --dataset parameter is not valid; try --dataset=compas or --dataset=adult")
@@ -291,8 +298,6 @@ def main(args):
     x_tensor = torch.tensor(df_binary.to_numpy().astype(np.float32))
     y_tensor = torch.tensor(Y.reshape(-1, 1).astype(np.float32))
     # Just duplicate y twice to maintain correct order
-    # l_tensor = torch.tensor(Y_true.to_numpy().reshape(-1, 1).astype(np.float32))
-    l_tensor = torch.tensor(Y.reshape(-1, 1).astype(np.float32))
     s_tensor = torch.tensor(preprocessing.OneHotEncoder().fit_transform(np.array(S).reshape(-1, 1)).toarray())
 
     dataset = TensorDataset(x_tensor, y_tensor, l_tensor, s_tensor)  # dataset = CustomDataset(x_tensor, y_tensor)